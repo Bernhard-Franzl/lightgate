2b7ba69f95bf627e8fed3eab386630b073e914fa
Bernhard-Franzl <75139279+Bernhard-Franzl@users.noreply.github.com>
2024-04-08 08:54:42 +0200

    Update archive_management.sh

--- archive_management/archive_management.sh
+++ archive_management/archive_management.sh
@@ -7,9 +7,9 @@ if sshpass -p Ha1lll1oo1 ssh pi_server@masterarbeit.ddns.net -p 8080  true; then
     echo "webserver is alive"
     python3 $working_directory/archive_management/archive_management.py
     if sshpass -p Ha1lll1oo1 rsync -aPvbc -e 'ssh -p 8080' $working_directory/archive/ pi_server@masterarbeit.ddns.net:archive/; then
-        systemctl stop ir_receiver.service
+        systemctl stop receiver.service
         rm -r $working_directory/data_$ROOMNAME
-        systemctl start ir_receiver.service
+        systemctl start receiver.service
     else
         echo "sync not successful"
     fi
